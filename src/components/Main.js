a3199c7fcfbc5d204bcfb2d1d8b90d557cd786b6
Paul Devlin <pmdevlin360@gmail.com>
2022-03-23 19:55:06 -0500

    object renamed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,15 @@
 import React from "react";
 import QrCode from '../images/smallQR.png'
+import info from '../utils/info'
 
 
 export default function Main(){
     return(
         <div className="main">
            <h2 className="aboutTitle">About</h2>
-           <p className="aboutText">I am a full stack software engineer, my native language is JavaScript.
-               I have a particular interest in game development and blockchain concepts.
-               I try to improve my skills daily and I am always trying to learn new things 
-               and more than willing to go down any rabbit hole with most technology's. 
-           </p>
+           <p className="aboutText">{info.bio}</p>
            <h2 className="interestsTitle">Interests</h2>
-           <p className="interestsText"> Software Engineering. Game programing and design. Financial study. Reader. Golfing enthusiast. Anything water related.</p>
+           <p className="interestsText">{info.interest}</p>
            <div className="qrContainer">
                <h3 className="QRtitle">Linkedin QRcode </h3>
                <img className="qrCode" src={QrCode} alt=""/>
--- src/utils/about.js
+++
@@ -1,7 +0,0 @@
-const infoObj = {}
-
-about.bio = 'I am a husband, father, veteran of the USMC and U.S. Army, and Software Engineer. My most recent experience is working in the PERN stack. I have an interest in blockchain concepts, game development, and full stack development for web applications. I am currently learning more about the deployment process of large scale applications as well as implementing a CI/CD pipeline. I am currently learning objective is to add Python, MongoDB, AWS, docker and more complex algorithms to my set of skills.'
-
-about.interest = 'Software engineering, game programing and design, financial studies, reading, family time and playing golf when I get free time'
-
-export default infoObj
