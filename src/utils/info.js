fefdb57e20d8ac91d3b93c80000c3cac4dd03ee7
Paul Devlin <pmdevlin360@gmail.com>
2022-03-24 09:29:48 -0500

    spelling mistakes

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,7 +1,7 @@
 const infoObj = {}
 
-infoObj.bio = 'I am a husband, father, veteran of the USMC and U.S. Army, and Software Engineer. My most recent experience is working in the PERN stack. I have an interest in blockchain concepts, game development, and full stack development for web applications. I am currently learning more about the deployment process of large scale applications as well as implementing a CI/CD pipeline. I am currently learning objective is to add Python, MongoDB, AWS, docker and more complex algorithms to my set of skills.'
+infoObj.bio = 'I am a husband, father, veteran of the USMC and U.S. Army, and Software Engineer. My most recent experience is working in the PERN stack. I have an interest in blockchain concepts, game development, and full stack development for web applications. I am currently learning more about the deployment process of large scale applications as well as implementing a CI/CD pipeline. I am currently working to add Python, MongoDB, AWS, docker and more complex algorithms to my set of skills.'
 
-infoObj.interest = 'Software engineering, game programing and design, financial studies, reading, family time and playing golf when I get free time'
+infoObj.interest = 'Software engineering, game programing and design, financial studies, reading, family time and playing golf when I get free time.'
 
 export default infoObj
